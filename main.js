be432f627282394f67b8742a2939f913fd1ff325
Gregory Anderson <anderson.gregory.mail@gmail.com>
2020-03-30 11:40:42 -0600

    complete display

---
+++ main.js
@@ -0,0 +1,54 @@
+let api = {
+  key: "15b2983e6d7980c12cd68cd4e441ffd1",
+  baseurl: "https://api.openweathermap.org/data/2.5/"
+}
+
+const searchbox = document.querySelector('.search-box');
+searchbox.addEventListener('keypress', setQuery);
+
+function setQuery (e) {
+   if (e.keyCode == 13) {
+     getResults(searchbox.value)
+   }
+}
+
+function getResults (query) {
+  let url = `${api.baseurl}weather?q=${query}&units=imperial&APPID=${api.key}`
+  console.log('url', url)
+  fetch(url)
+  .then(weather => {
+    return weather.json();
+  })
+  .then(displayResults)
+}
+
+function displayResults(weather){
+  console.log(weather)
+  let city = document.querySelector('.location .city')
+  city.innerText = `${weather.name}, ${weather.sys.country}`
+
+  let now = new Date()
+  let date = document.querySelector('.location .date ')
+  date.innerText = dateBuilder(now)
+
+  let temp = document.querySelector('.current .temp')
+  temp.innerHTML = `${Math.round(weather.main.temp)}<span>°f</span>`
+
+  let condition = document.querySelector('.current .weather')
+  condition.innerText = weather.weather[0].main;
+
+  let hilow = document.querySelector('.hi-low')
+  hilow.innerText = `${Math.round(weather.main.temp_min)}°f / ${Math.round(weather.main.temp_max)}°f`
+} 
+
+function dateBuilder(d) {
+  let months = ['January', 'February','March','April','May','June','July','August','September','October','November','December']
+  let days = ['Sunday', 'Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
+
+  let day = days[d.getDay()]
+  let date = d.getDate()
+  let month = months[d.getMonth()]
+  let year = d.getFullYear()
+
+  return `${day} ${date} ${month} ${year}`
+}
